c0cf85d870112824af6afcfd7a4b83b6fa53c4b4
Dan Beam <dan@danbeam.org>
2012-03-05 03:18:10 -0800

    jenkins actually works now?

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -z "$JENKINS_URL" ] || [ ! -z "$HUDSON_URL" ]; then
-  mkdir -p coverage/html
-  phpunit test/test_suite.php --coverage-html coverage/html --coverage-xml coverage/clover.xml 2>&1
-else
-  phpunit tests/test_suite.php 2>&1
+conf="phpunit.local.xml";
+if [ ! -z "$JENKINS_URL" ]; then
+  conf="phpunit.jenkins.xml";
 fi
+
+phpunit -c "$conf"
